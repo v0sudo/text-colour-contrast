45c8c9212b9761c87c1619167fc0b8ac963f0fb9
Luke <73408132+v0sudo@users.noreply.github.com>
2025-12-29 18:30:04 +0000

    Fix spelling of 'Colour' in function name

--- text-colour-contrast.ts
+++ text-colour-contrast.ts
@@ -1,4 +1,4 @@
-export function textColorContrast(color: string) {
+export function textColourContrast(color: string) {
   const r = parseInt(color.slice(1, 3), 16);
   const g = parseInt(color.slice(3, 5), 16);
   const b = parseInt(color.slice(5, 7), 16);
