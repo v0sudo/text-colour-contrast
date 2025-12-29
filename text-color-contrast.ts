58638767a1c8343d1807b8671224455b89e20c4e
Luke <73408132+v0sudo@users.noreply.github.com>
2025-12-29 18:27:01 +0000

    Add textColorContrast function for color contrast

---
+++ text-color-contrast.ts
@@ -0,0 +1,7 @@
+export function textColorContrast(color: string) {
+  const r = parseInt(color.slice(1, 3), 16);
+  const g = parseInt(color.slice(3, 5), 16);
+  const b = parseInt(color.slice(5, 7), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "black" : "white";
+}
